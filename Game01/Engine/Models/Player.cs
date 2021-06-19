2591a4b957852d86bc3bd5d44f6ab0463829582d
Vinicius Murta <vinimurta4740@gmail.com>
2021-06-18 21:28:01 -0300

    Add model player

---
+++ Game01/Engine/Models/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        private String nome;
+
+        private String classe;
+
+        private int hitPoints;
+
+        private int exp;
+
+        private float level;
+
+        private int gold;
+
+
+        public void set(Player model)
+        {
+            this.nome = model.nome;
+            this.classe = model.classe;
+            this.hitPoints = model.hitPoints;
+            this.exp = model.exp;
+            this.level = model.level;
+            this.gold = model.gold;
+
+        }
+
+        public String getNome()
+        {
+            return nome;
+        }
+
+        public String getClasse()
+        {
+            return classe;
+        }
+
+        public int getHitPionts()
+        {
+            return hitPoints;
+        }
+
+        public int getExp()
+        {
+            return exp;
+        }
+
+        public float getLevel()
+        {
+            return level;
+        }
+
+        public int getGold()
+        {
+            return gold;
+        }
+
+    }
+}
