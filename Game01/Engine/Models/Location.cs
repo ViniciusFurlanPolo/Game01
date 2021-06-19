3316546e079ee24ba2b78d58c983fa98194e9452
Vinicius Murta <vinimurta4740@gmail.com>
2021-06-19 00:31:49 -0300

    Adiciona Locais

---
+++ Game01/Engine/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Location
+    {
+        public int coordenadaX { get; set; }
+        public int coordenadaY { get; set; }
+        public String lugar { get; set; }
+        public String descricao { get; set; }
+        public String imageFile { get; set; }
+    }
+}
--- Game01/Engine/ViewModels/GameSession.cs
+++ Game01/Engine/ViewModels/GameSession.cs
@@ -8,6 +8,7 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player PlayerAtual { get; set; }
+        public Location LocalAtual { get; set; }
 
         public GameSession()
         {
@@ -18,6 +19,14 @@ namespace Engine.ViewModels
             PlayerAtual.hitPoints = 10;
             PlayerAtual.exp = 0;
             PlayerAtual.level = 1;
+
+            LocalAtual = new Location();
+            LocalAtual.lugar = "Home";
+            LocalAtual.coordenadaX = 0;
+            LocalAtual.coordenadaY = -1;
+            LocalAtual.descricao = "This is your house";
+            LocalAtual.imageFile = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
+
         }
     }
 }
