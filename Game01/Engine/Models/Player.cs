1346c0da0eb073fb5ca1d626cfb4e60f1e1826f6
Vinicius Murta <vinimurta4740@gmail.com>
2021-06-18 23:35:11 -0300

    Atualização player

--- Game01/Engine/Models/Player.cs
+++ Game01/Engine/Models/Player.cs
@@ -1,22 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
-        public String nome { get; set; }
 
-        public String classe { get; set; }
+        private String Nome;
 
-        public int hitPoints { get; set; }
+        private String Classe;
 
-        public int exp { get; set; }
+        private int HP;
 
-        public int level { get; set; }
+        private int Xp;
 
-        public int gold { get; set; }
+        private int Level;
+
+        private int Gold;
+
+        public String nome 
+        {
+            get { return Nome; } 
+            set 
+            {
+                Nome = value;
+                OnPropertyChanged("nome");
+            }
+        }
+
+        public String classe 
+        { 
+            get { return Classe; }
+            set 
+            {
+                Classe = value;
+                OnPropertyChanged("classe");
+            }
+        }
+
+        public int hitPoints 
+        { 
+            get { return HP; }
+            set
+            {
+                HP = value;
+                OnPropertyChanged("hitPoints");
+            }
+        }
+
+        public int exp 
+        {
+            get { return Xp; }
+            set 
+            {
+                Xp = value;
+                OnPropertyChanged("exp");
+            }
+        }
+
+        public int level 
+        {
+            get { return Level; } 
+            set
+            {
+                Level = value;
+                OnPropertyChanged("level");
+            }
+        }
+
+        public int gold 
+        {
+            get { return Gold; }
+            set
+            {
+                Gold = value;
+                OnPropertyChanged("gold");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(String PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
 
     }
 }
